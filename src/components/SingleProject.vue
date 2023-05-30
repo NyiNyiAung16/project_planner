2767173309b26289a53f617b5c8e3f7c30987f32
nyinyi <nyinyi31088@gmail.com>
2023-05-30 21:25:56 +0630

    home and single project

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="main" @click="showDetail=!showDetail">
+        <h2>{{project.title}}</h2>
+        <p v-if="showDetail">{{project.detail}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            showDetail:false
+        }
+    }
+}
+</script>
+
+<style>
+    .main{
+        background-color: rgb(203, 197, 197);
+        padding: 20px;
+        margin-bottom: 10px;
+        border-radius: 4px;
+    }
+    h2{
+        color: indigo;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Projects Planner</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project"></SingleProject>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleProject from "../components/SingleProject.vue"
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    SingleProject
+  },
+  data(){
+    return{
+      projects:[]
+    }
+  },
+  mounted(){
+    fetch("http://localhost:3000/projects")
+    .then((response)=>{
+      return response.json()
+    })
+    .then((datas)=>{
+      this.projects=datas
+    })
+    .catch(()=>{
+
+    })
   }
 }
 </script>
